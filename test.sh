d09a70190f75829ad28995e86f5e628df4ce6e36
Tuowei Wang <wtw18@mails.tsinghua.edu.cn>
2023-06-10 02:26:38 +0000

    Add: baseline

---
+++ test.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES="0" python3.8 test.py  \
+--input_path ../data/val_A_labels_resized \
+--img_path ../data/train_resized/imgs \
+--which_epoch 190
---
+++ train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES="0" python3.8 train.py --input_path ../data/train_resized
